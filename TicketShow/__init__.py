f9185e3fc486766f48ef17507d5e55c9e275a07a
SouvikChoudhury360 <choudhurysouvik2003@gmail.com>
2023-07-30 11:52:31 +0530

    cors added

--- TicketShow/__init__.py
+++ TicketShow/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -11,6 +12,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     db.session.close_all()
     db.init_app(app)
+
+    CORS(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
